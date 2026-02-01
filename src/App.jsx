a947c4d7a69a5f9af781d44dbf2b8126106dd2c8
Cooper Can <coopercan@Mac-mini-tilhrende-Cooper.local>
2026-02-01 17:37:26 +0100

    Add GDPR-compliant cookie consent banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './index.css'
+import CookieConsent from './CookieConsent'
 
 function App() {
   return (
@@ -183,6 +184,9 @@ function App() {
           </div>
         </div>
       </footer>
+
+      {/* Cookie Consent */}
+      <CookieConsent />
     </div>
   )
 }
